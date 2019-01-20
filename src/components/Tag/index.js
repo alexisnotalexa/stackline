f625c52f2fc75f4100199b11a2364039fd64b6af
Alexis Okamura <alexiskokamura@gmail.com>
2019-01-20 12:32:32 -0800

    create tag component

---
+++ src/components/Tag/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Tag = ({ name }) => {
+  return (
+    <div>
+      <span>{name}</span>
+    </div>
+  );
+};
+
+export default Tag;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Tag from '../../components/Tag';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Tag name={'Pantry'} />
       </div>
     );
   }
