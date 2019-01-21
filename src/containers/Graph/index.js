7301a3e5b95146001a746a5107cfa85ddacb3c6f
Alexis Okamura <alexiskokamura@gmail.com>
2019-01-20 17:05:40 -0800

    clean up styles in graph component

--- src/containers/Graph/index.js
+++ src/containers/Graph/index.js
@@ -8,21 +8,30 @@ class Graph extends Component {
     data: PropTypes.object
   };
 
+  renderMonths = () => {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return (
+      <div className={'graph--months'}>
+        {months.map(month => <span className={'graph--text'}>{month}</span>)}
+      </div>
+    );
+  }
+
   render() {
     const { data } = this.props;
     const parseDate = d3.timeParse('%Y-%m-%d');
 
     const x = d3.scaleLinear()
       .domain(d3.extent(data, d => parseDate(d.weekEnding)))
-      .range([0, 775]);
+      .range([0, 750]);
 
     const y = d3.scaleLinear()
-      .domain([150, d3.max(data, d => d.retailSales)])
-      .range([200, 150]);
+      .domain([100, d3.max(data, d => d.retailSales)])
+      .range([150, 100]);
     
     const y2 = d3.scaleLinear()
-      .domain([200, d3.max(data, d => d.wholesaleSales)])
-      .range([250, 200]);
+      .domain([150, d3.max(data, d => d.wholesaleSales)])
+      .range([200, 150]);
     
     const retailLine = d3.line()
       .x(data => x(parseDate(data.weekEnding)))
@@ -35,10 +44,14 @@ class Graph extends Component {
       .curve(d3.curveCatmullRom.alpha(0.5));
     
     return (
-      <svg width={775} height={500}>
-        <path className={'retail-sales'} d={retailLine(data)} />
-        <path className={'wholesale-sales'} d={wholesaleLine(data)} />
-      </svg>
+      <div className={'graph'}>
+        <span className={'graph--text'}>Retail Sales</span>
+        <svg width={750} height={300}>
+          <path className={'retail-sales'} d={retailLine(data)} />
+          <path className={'wholesale-sales'} d={wholesaleLine(data)} />
+        </svg>
+        {this.renderMonths()}
+      </div>
     );
   }
 }
